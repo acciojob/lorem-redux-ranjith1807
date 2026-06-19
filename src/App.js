ce8771d40ecf6994deef2d1a83ca718c2d4123e1
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-06-19 12:28:54 +0530

    sfsfs

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         <h1>A short Naration of Lorem Ipsum</h1>
         <h4>
           Below Contains A title and Body gotten from
-          <br />a random API, Please take your time to Review
+          a random API, Please take your time to Review
         </h4>
       </header>
 
