db51742c01901287932b4dd686426df4ba0b7fd5
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-07-20 11:37:12 +0530

    j

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; // Use 'react-dom/client' if using React 18+
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import {store} from './redux/store';
 import App from './App';
 import './App.css';
 
