caa0f3db7508efced9a213d5aa5ac13e617cdb38
ranjith1807 <v.ranjith9095490240@gmail.com>
2026-07-21 21:19:42 +0530

    dfdf

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,8 @@ import { fetchLorem } from "../store/actions";
 
 const App = () => {
   const dispatch = useDispatch();
-  const { loading, data, error } = useSelector((state) => state);
+  const loremState = useSelector((state) => state.lorem ?? state);
+  const { loading, data, error } = loremState;
 
   useEffect(() => {
     dispatch(fetchLorem());
@@ -18,28 +19,23 @@ const App = () => {
         <h4>Loading...</h4>
       ) : (
         <h4>
-          Below Contains A title and Body gotten froma random API, Please take your time to Review
+          Below Contains A title and Body gotten from a random API, Please take
+          your time to Review
         </h4>
       )}
 
       {error && <h4>Error: {error}</h4>}
 
       <ul>
-        {loading ? (
-          <li>
-            <p className="id">Loading id</p>
-            <p className="title">Title :Loading tiltes</p>
-            <p className="body">Body :Loading body</p>
-          </li>
-        ) : (
-          data && (
-            <li>
-              <p className="id">{data.id}</p>
-              <p className="title">Title :{data.title}</p>
-              <p className="body">Body :{data.body}</p>
-            </li>
-          )
-        )}
+        <li>
+          <p className="id">{loading ? "Loading id" : data?.id}</p>
+          <p className="title">
+            Title :{loading ? "Loading title" : data?.title}
+          </p>
+          <p className="body">
+            Body :{loading ? "Loading body" : data?.body}
+          </p>
+        </li>
       </ul>
     </div>
   );
